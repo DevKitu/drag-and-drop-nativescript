5bd4a9f1d601fd78f8e5a14451c28249806cf08c
DevKitu <44010881+DevKitu@users.noreply.github.com>
2020-07-20 01:49:28 -0500

    Update item-detail.component.ts
    One very important note for Angular guys, the piece of code inside the ngOnInit MUST move to ngAfterViewInit() bcs the view must be initialized before you can access the viewChild. I added the ngAfterViewInit()

--- src/app/modules/item/item-detail.component.ts
+++ src/app/modules/item/item-detail.component.ts
@@ -35,6 +35,8 @@ export class ItemDetailComponent implements OnInit {
 
   ngOnInit(): void {
     this.getItemData();
+  }
+  ngAfterViewInit() {
     this.setImagePosition();
   }
   setImagePosition() {
